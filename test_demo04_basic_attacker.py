edcf6986b273debac38ce2d1084a7e297f3880fa
ASPP Student <student@localhost>
2022-09-09 18:00:01 +0200

    Added feature to pick the closes enemy if both enemies are not noisy

--- test_demo04_basic_attacker.py
+++ test_demo04_basic_attacker.py
@@ -37,3 +37,13 @@ def test_do_not_step_on_enemy():
     next_move = move(bot, {})
     assert next_move == (5, 2)
 
+def test_escape_mode_given_enemy_steps():
+    layout="""
+    ########
+    #    x.#
+    #.b a y#
+    ########
+    """
+    bot = setup_test_game(layout=layout, is_blue=True)
+    next_move = move(bot, {})
+    assert next_move == (3, 2)
