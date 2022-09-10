9bbe0cee5a37831c577e84d18f0ab151ef85c408
ASPP Student <student@localhost>
2022-09-10 11:58:18 +0200

    DE: move initialization from attacker.py

--- group2.py
+++ group2.py
@@ -4,11 +4,11 @@ import networkx
 import pelita.utils
 from pelita.utils import walls_to_graph
 
-from demo05_basic_defender import move as move_defender
-from demo04_basic_attacker import move as move_attacker
+#from demo05_basic_defender import move as move_defender
+#from demo04_basic_attacker import move as move_attacker
 
-#from defender_02 import move as move_defender
-#from attacker_02 import move as move_attacker
+from defender_02 import move as move_defender
+from attacker import move as move_attacker, border
 
 #import utils
 
@@ -28,6 +28,9 @@ def init_state(personality):
         "defend_target": None,
         "defend_path": None,
 
+        #"path": [],
+        #"border": [],
+        #"graph": [],
 
         # # entries about previous food postions
         # "prev_foodmap": [],
@@ -53,17 +56,22 @@ def move(bot, state):
         state['graph'] = walls_to_graph(bot.walls)
         state['prev_foodmap'] = bot.food # initial state
         state['eaten_food'] = []
-
+        our_border = border(bot)
+        state['border'] = our_border
         #### UPDATE ACCORDING TO STRATEGY #####
         state["global_strategy"] = "game_start" ## not used right now, keep for later?
 
         state[0] = init_state("attacker")
         state[1] = init_state("attacker")
 
-## if conditions for each global_strategy
+        state[0]["defend_food_path"] = []
+        state[1]["defend_food_target"] = []
+
+# if conditions for each global_strategy
     if bot.round == 16: ## TODO: find better condition for switiching out of initial strategy
         state["global_strategy"] = "individual" # each bot's personality is updated individually
-    elif len(bot.food) <= 4:
+
+    if len(bot.food) <= 0:
         state["global_strategy"] = "defend_last_food"
 ### define what to do based on global_strategy
 # game start --> move to midline
@@ -80,43 +88,36 @@ def move(bot, state):
         state[1]["personality"] = "defender"
 
     elif state["global_strategy"] == "defend_last_food":
+        print("entered defend last food state")
         target_options = bot.food
         # figure out how far each food is
         # for loop over target_options -- distance/shortest attack_path
         # pick shortest path target
-        possible_paths = {}
-        for target_option in target_options:
-            possible_paths[target_option] = len(networkx.shortest_path(state['graph'], bot.position, target)[1:])
+        if not state[bot.turn]["defend_food_target"]:
+            possible_paths = {}
+            for target_option in target_options:
+                possible_paths[target_option] = len(networkx.shortest_path(state['graph'], bot.position, target)[1:])
 
-        closest_food = sorted(possible_paths.items(), key=lambda x: x[1], reverse=True)
-        state[bot.turn]["defend_path"] = closest_food[0]
+            closest_food = sorted(possible_paths.items(), key=lambda x: x[1])
+            state[bot.turn]["defend_food_target"] = closest_food[0]
+            state[bot.turn]["defend_food_path"] = networkx.shortest_path(state['graph'], bot.position, closest_food[0])[1:]
 
+        print(closest_food[0], closest_food)
         # choose a target food pellet if we still don't have one or
         ## if the old target has been already eaten
 
-    #if (target is None) or (target not in enemy[0].food):
-        # position of the target food pellet
-        #target = bot.random.choice(bot.food)
-        # use networkx to get the shortest path from here to the target
-        # we do not use the first position, which is always equal to bot_position
-        #path = networkx.shortest_path(state['graph'], bot.position, target)[1:]
-        state[bot.turn]["defend_path"] = path
-        state[bot.turn]["defend_target"] = target
-
+        path = state[bot.turn]["defend_food_path"]
         # get the next position along the shortest path to reach our target
         next_pos = path.pop(0)
-        # if we are not in our homezone we should check if it is safe to proceed
-        if next_pos in bot.homezone:
-            # get a list of safe positions
-            safe_positions = []
-            for pos in bot.legal_positions:
-                if pos not in (enemy[0].position, enemy[1].position):
-                    safe_positions.append(pos)
+
+        # get a list of safe positions
+        safe_positions = []
+        for pos in bot.legal_positions:
+            if pos not in (enemy[0].position, enemy[1].position):
+                safe_positions.append(pos)
 
 
-
-
-    # keep track of food changes for distant enemy positions
+    #### keep track of food changes for distant enemy positions
     track_foodchange(bot, state)
     # update previous food map
 
